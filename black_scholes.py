7f276d0b40f70805713f78d873d6356d428a7056
Rishabh Ballal <rishabhballal@gmail.com>
2025-11-21 15:52:04 +0530

    add option greeks

--- black_scholes.py
+++ black_scholes.py
@@ -16,6 +16,9 @@ def auxiliary_vars(func):
         return func(S,K,t,r,d,v,d1,d2)
     return wrapper
 
+
+# vanilla options
+
 vanilla_call = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
     zc_bond(t,r)*(F(S,t,r,d)*norm.cdf(d1) - K*norm.cdf(d2)))
 
@@ -30,3 +33,44 @@ digital_call = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
 digital_put = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
     zc_bond(t,r)*norm.cdf(-d2))
     # zc_bond(t,r) - digital_call(S,K,t,r,d,v)
+
+
+# Greeks of a vanilla call option
+
+n = lambda x: math.exp(-x**2/2)/math.sqrt(2*math.pi)
+
+delta_call = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    math.exp(-d*t)*norm.cdf(d1))
+
+gamma_call = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    (math.exp(-d*t)*n(d1))/(S*v*math.sqrt(t)))
+
+vega_call = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    S*math.exp(-d*t)*n(d1))
+
+rho_call = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    K*zc_bond(t,r)*t*norm.cdf(d2))
+
+theta_call = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    S*math.exp(-d*t)*d*norm.cdf(d1) - K*zc_bond(t,r)*r*norm.cdf(d2) - \
+    S*math.exp(-d*t)*v*n(d1)/(2*math.sqrt(t)))
+
+
+# Greeks of a vanilla put option
+
+delta_put = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    -math.exp(-d*t)*norm.cdf(-d1))
+    # delta_call(S,K,t,r,d,v) - math.exp(-d*t)
+
+gamma_put = gamma_call
+
+vega_put = vega_call
+
+rho_put = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    -K*zc_bond(t,r)*t*norm.cdf(-d2))
+    # rho_call(S,K,t,r,d,v) - K*zc_bond(t,r)*t
+
+theta_put = auxiliary_vars(lambda S,K,t,r,d,v,d1,d2: \
+    -S*math.exp(-d*t)*d*norm.cdf(-d1) + K*zc_bond(t,r)*r*norm.cdf(-d2) - \
+    S*math.exp(-d*t)*v*n(d1)/(2*math.sqrt(t)))
+    # theta_call(S,K,t,r,d,v) - (S*math.exp(-d*t)*d - K*zc_bond(t,r)*r)
