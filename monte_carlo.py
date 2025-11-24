e77d67526f9303cdc5248d369484ac82c0e8041e
Rishabh Ballal <rishabhballal@gmail.com>
2025-11-24 19:39:31 +0530

    feat: add methods for option greeks

--- monte_carlo.py
+++ monte_carlo.py
@@ -3,69 +3,87 @@ import numpy as np
 
 rng = np.random.default_rng()
 
-class StockPaths:
-    def __init__(self, count, S_0=100, t=1, r=0.05, d=0.03, v=0.10):
+class StockPath:
+    def __init__(self, count=10**6, spot=100, rate=0.05, divid=0.03, vol=0.10):
         self.count = count
-        self.S_0 = S_0
-        self.t = t
-        self.r = r
-        self.d = d
-        self.v = v
+        self.spot = spot
+        self.rate = rate
+        self.divid = divid
+        self.vol = vol
 
-    def gbm_solution(self, t=1, rands=[]):
-        if not len(rands):
-            rands = rng.standard_normal(self.count)
-        return [self.S_0*math.exp((self.r - self.d)*self.t - \
-            0.5*(self.v**2)*self.t + self.v*math.sqrt(self.t)*x) for x in rands]
-
-    def gbm_steps(self, t=1, N=10**4, rands=[]):
-        if not len(rands):
-            rands = rng.standard_normal((N,self.count))
-        S_t = np.ones((N+1,self.count))*self.S_0
-        for i in range(N):
-            S_t[i+1] = S_t[i]*(1 + (self.r - self.d)*self.t/N + \
-                self.v*math.sqrt(self.t/N) * rands[i])
+    def geom_brownian(self, time=1, steps=1, rand=[]):
+        if not len(rand):
+            rand = rng.standard_normal((steps, self.count))
+        S_t = np.ones((steps+1,self.count))*self.spot
+        if not steps-1:
+            S_t[-1] = [self.spot*math.exp((self.rate - self.divid)*time - \
+                0.5*(self.vol**2)*time + self.vol*math.sqrt(time)*x) \
+                for x in rand[-1]]
+        else:
+            for i in range(steps):
+                S_t[i+1] = S_t[i]*(1 + (self.rate - self.divid)*time/steps + \
+                    self.vol*math.sqrt(time/steps)*rand[i])
         return S_t
 
-class OptionValues:
-    def __init__(self, S_0, t, r, d, v, payoff):
-        self.S_0 = S_0
-        self.t = t
-        self.r = r
-        self.d = d
-        self.v = v
-        self.payoff = payoff
+class EuropeanOption:
+    def __init__(self, stock, expiry, payoff):
+        self.stock = stock
+        self.expiry = expiry
+        self.payoff = lambda S: np.array([payoff(x) for x in S])
 
-    # def price(self, count, N=1):
-    #     S_t = self.asset_singlestep(count) if N == 1 \
-    #         else self.asset_multistep(count, N)
-    #     return np.mean([math.exp(-self.r*self.t) * self.payoff(x) for x in S_t])
+    def _random_seed(func):
+        def wrapper(self, steps=1, rand=[]):
+            if not len(rand):
+                rand = rng.standard_normal((steps, self.stock.count))
+            return func(self, steps, rand)
+        return wrapper
 
-    def price2(self, count):
-        rands = rng.standard_normal(count)
-        S = [self.S_0*math.exp((self.r - self.d)*self.t - \
-            0.5*(self.v**2)*self.t + self.v*math.sqrt(self.t)*x) \
-            for x in rands]
-        S_delta = [(self.S_0+0.001)*math.exp((self.r - self.d)*self.t - \
-            0.5*(self.v**2)*self.t + self.v*math.sqrt(self.t)*x) \
-            for x in rands]
-        S_gamma = [(self.S_0+0.001)*math.exp((self.r - self.d)*self.t - \
-            0.5*(self.v**2)*self.t + self.v*math.sqrt(self.t)*x) \
-            for x in rands]
-        S_vega = [(self.S_0+0.001)*math.exp((self.r - self.d)*self.t - \
-            0.5*(self.v**2)*self.t + self.v*math.sqrt(self.t)*x) \
-            for x in rands]
-        S_rho = [(self.S_0+0.001)*math.exp((self.r - self.d)*self.t - \
-            0.5*(self.v**2)*self.t + self.v*math.sqrt(self.t)*x) \
-            for x in rands]
-        S_theta = [(self.S_0+0.001)*math.exp((self.r - self.d)*self.t - \
-            0.5*(self.v**2)*self.t + self.v*math.sqrt(self.t)*x) \
-            for x in rands]
+    price = lambda self: math.exp(-self.stock.rate*self.expiry) * \
+        np.mean(self.payoff(self.stock.gbm_solution(self.expiry)))
 
-        self.price = np.mean([math.exp(-self.r*self.t) * self.payoff(S[i]) for i in range(count)])
-        self.delta = np.mean([math.exp(-self.r*self.t) * (self.payoff(S_delta[i]) - self.payoff(S[i]))/0.001 for i in range(count)])
-        self.gamma = np.mean([math.exp(-self.r*self.t) * (self.payoff(S_delta[i]) - self.payoff(S[i]))/0.001 for i in range(count)])
-        self.vega = np.mean([math.exp(-self.r*self.t) * (self.payoff(S_delta[i]) - self.payoff(S[i]))/0.001 for i in range(count)])
-        self.rho = np.mean([math.exp(-self.r*self.t) * (self.payoff(S_delta[i]) - self.payoff(S[i]))/0.001 for i in range(count)])
-        self.theta = np.mean([math.exp(-self.r*self.t) * (self.payoff(S_delta[i]) - self.payoff(S[i]))/0.001 for i in range(count)])
-        return self.price
+    @_random_seed
+    def delta(self, steps, rand):
+        epsilon = 0.01
+        S_1 = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
+        self.stock.spot += epsilon
+        S_2 = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
+        self.stock.spot -= epsilon
+        return math.exp(-self.stock.rate*self.expiry) * \
+            np.mean(self.payoff(S_2) - self.payoff(S_1))/epsilon
+
+    @_random_seed
+    def gamma(self, steps, rand):
+        epsilon = 0.01
+        delta_1 = self.delta(steps, rand)
+        self.stock.spot += epsilon
+        delta_2 = self.delta(steps, rand)
+        self.stock.spot -= epsilon
+        return (delta_2 - delta_1)/epsilon
+
+    @_random_seed
+    def vega(self, steps, rand):
+        epsilon = 0.001
+        S_1 = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
+        self.stock.vol += epsilon
+        S_2 = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
+        self.stock.vol -= epsilon
+        return math.exp(-self.stock.rate*self.expiry) * \
+            np.mean(self.payoff(S_2) - self.payoff(S_1))/epsilon
+
+    @_random_seed
+    def rho(self, steps, rand):
+        epsilon = 0.001
+        S_1 = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
+        self.stock.rate += epsilon
+        S_2 = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
+        self.stock.rate -= epsilon
+        return math.exp(-self.stock.rate*self.expiry) * \
+            np.mean(self.payoff(S_2) - self.payoff(S_1))/epsilon
+
+    @_random_seed
+    def theta(self, steps, rand):
+        epsilon = 0.01
+        S_1 = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
+        S_2 = self.stock.geom_brownian(self.expiry-epsilon, steps, rand)[-1]
+        return math.exp(-self.stock.rate*self.expiry) * \
+            np.mean(self.payoff(S_2) - self.payoff(S_1))/epsilon
