e348d3845ac905699300965c977cb99525ad67a4
Rishabh Ballal <rishabhballal@gmail.com>
2025-11-30 18:36:12 +0530

    feat: add class for Bermudan options

--- trees.py
+++ trees.py
@@ -10,7 +10,7 @@ class Stock:
         self.divid = divid
         self.vol = vol
 
-    def geom_brownian(self, time=1, steps=500):
+    def geom_brownian(self, time=1, steps=1000):
         self.up = math.exp(self.vol * math.sqrt(time / steps))
         self.down = 1/self.up
         self.pr = (math.exp(self.rate * time / steps) - self.down) / \
@@ -32,9 +32,9 @@ class EuropeanOption:
         value = [self.payoff(x) for x in S]
         for i in reversed(range(steps)):
             for j in range(i+1):
-                value[i][j] = math.exp(-self.stock.rate * self.expiry / steps) \
-                    * (self.stock.pr * value[i+1][j] + (1 - self.stock.pr) * \
-                    value[i+1][j+1])
+                value[i][j] = math.exp(-self.stock.rate * self.expiry / \
+                    steps) * (self.stock.pr * value[i+1][j] + \
+                    (1 - self.stock.pr) * value[i+1][j+1])
         return value[0][0]
 
 class AmericanOption:
@@ -51,4 +51,28 @@ class AmericanOption:
                 value[i][j] = max(value[i][j], math.exp(-self.stock.rate * \
                     self.expiry / steps) * (self.stock.pr * value[i+1][j] + \
                     (1 - self.stock.pr) * value[i+1][j+1]))
+        return value[0][0]
+
+class BermudanOption:
+    def __init__(self, stock, times, payoff):
+        self.stock = stock
+        self.times = times
+        self.payoff = lambda S: [payoff(x) for x in S]
+
+    def price(self, steps=100):
+        total_steps = steps * len(self.times)
+        dt = self.times[-1] / total_steps
+        S = self.stock.geom_brownian(self.times[-1], total_steps)
+        value = [self.payoff(x) for x in S]
+        for i in reversed(range(total_steps)):
+            if i*dt in self.times:
+                for j in range(i+1):
+                    value[i][j] = max(value[i][j], math.exp(-self.stock.rate * \
+                        dt) * (self.stock.pr * value[i+1][j] + \
+                        (1 - self.stock.pr) * value[i+1][j+1]))
+            else:
+                for j in range(i+1):
+                    value[i][j] = math.exp(-self.stock.rate * dt) * \
+                        (self.stock.pr * value[i+1][j] + (1 - self.stock.pr) * \
+                        value[i+1][j+1])
         return value[0][0]
